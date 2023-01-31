cde9cd1941c5484453a2f4bb89103b0beab85fb6
Yu Wu <yuwu@smail.nju.edu.cn>
2023-01-31 15:51:34 +0800

    add ParamCondLexer

--- src/main/java/pascal/taie/analysis/bugfinder/insecureapi/ParamCondLexer.java
+++ src/main/java/pascal/taie/analysis/bugfinder/insecureapi/ParamCondLexer.java
@@ -22,28 +22,60 @@
 
 package pascal.taie.analysis.bugfinder.insecureapi;
 
+import pascal.taie.util.AnalysisException;
+import pascal.taie.util.collection.Lists;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class ParamCondLexer {
 
-    private static final Rule[] rules = {
-            new Rule(Pattern.compile(" +"), ParamCondTokenType.NO_TYPE),
-            new Rule(Pattern.compile("p[1-9][0-9]*"), ParamCondTokenType.INDEX),
-            new Rule(Pattern.compile("/"), ParamCondTokenType.REGEX),
-            new Rule(Pattern.compile("="), ParamCondTokenType.EQ),
-            new Rule(Pattern.compile("!="), ParamCondTokenType.NEQ),
-            new Rule(Pattern.compile("&"), ParamCondTokenType.AND),
-            new Rule(Pattern.compile("\\|"), ParamCondTokenType.OR),
-            new Rule(Pattern.compile("\\("), ParamCondTokenType.LEFT_PARENTHESES),
-            new Rule(Pattern.compile("\\("), ParamCondTokenType.RIGHT_PARENTHESES),
-    };
+    private static final List<Rule> rules = List.of(
+            Rule.getRule(" +", ParamCondTokenType.NO_TYPE),
+            Rule.getRule("p[1-9][0-9]*", ParamCondTokenType.INDEX),
+            Rule.getRule("/((?:[^/\\\\]|\\\\.)*)/", ParamCondTokenType.REGEX),
+            Rule.getRule("=", ParamCondTokenType.EQ),
+            Rule.getRule("!=", ParamCondTokenType.NEQ),
+            Rule.getRule("&", ParamCondTokenType.AND),
+            Rule.getRule("\\|", ParamCondTokenType.OR),
+            Rule.getRule("\\(", ParamCondTokenType.LEFT_PARENTHESES),
+            Rule.getRule("\\(", ParamCondTokenType.RIGHT_PARENTHESES)
+            );
+
+    private ParamCondLexer() {
+    }
 
     public static List<ParamCondToken> analyze(String expr) {
-        // TODO
-        return null;
+        List<ParamCondToken> tokenList = new ArrayList<>();
+        List<Matcher> matchers = Lists.map(rules, rule -> rule.pattern.matcher(expr));
+        int position = 0;
+        outer:
+        while(position < expr.length()) {
+            for(int i = 0; i < rules.size(); i++) {
+                Matcher matcher = matchers.get(i);
+                if(matcher.find(position)) {
+                    ParamCondTokenType type = rules.get(i).type;
+                    switch(type) {
+                        case NO_TYPE -> {}
+                        case REGEX -> tokenList.add(new ParamCondToken(type,
+                                matcher.group(1).replace("\\/", "/")));
+                        default -> tokenList.add(new ParamCondToken(type, matcher.group()));
+                    }
+                    position = matcher.end();
+                    continue outer;
+                }
+            }
+            throw new AnalysisException("Failed to match tokens at position " + position + " in " + expr);
+        }
+        return tokenList;
     }
 
     private record Rule(Pattern pattern, ParamCondTokenType type) {
+
+        public static Rule getRule(String regex, ParamCondTokenType type) {
+            return new Rule(Pattern.compile(regex), type);
+        }
     }
 }
