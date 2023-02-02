104c3ea9fb48c1507b605a08f2dd504ccd86a1d4
Xiao Yinning <211220110@smail.nju.edu.cn>
2023-02-02 14:16:19 +0800

    add Insecureapi Junit

--- src/test/java/pascal/taie/analysis/bugfinder/BugFinderTestSuite.java
+++ src/test/java/pascal/taie/analysis/bugfinder/BugFinderTestSuite.java
@@ -31,6 +31,7 @@ import org.junit.runners.Suite;
         DroppedExceptionTest.class,
         IsNullTest.class,
         NullPointerDetectionTest.class,
+        InsecureAPITest.class,
 })
 public class BugFinderTestSuite {
 }
---
+++ src/test/java/pascal/taie/analysis/bugfinder/InsecureAPITest.java
@@ -0,0 +1,21 @@
+package pascal.taie.analysis.bugfinder;
+
+import org.junit.Test;
+import pascal.taie.analysis.Tests;
+import pascal.taie.analysis.bugfinder.insecureapi.InsecureAPIUsageDetector;
+
+public class InsecureAPITest {
+
+    private static final String folderPath = "src/test/resources/bugfinder/InsecureAPI";
+
+    void testInsecureAPI(String inputClass)
+    {
+        Tests.testInput(inputClass, folderPath, InsecureAPIUsageDetector.ID, "path: src/main/resources/insecureapi");
+    }
+
+    @Test
+    public void test() {
+        testInsecureAPI("Random");
+    }
+
+}
